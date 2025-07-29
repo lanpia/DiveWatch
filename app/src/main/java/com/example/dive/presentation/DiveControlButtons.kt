3f7b9da2eac68008da426a409f072dd6ebc9c3ec
lanpia <roina1108@gmail.com>
2025-07-29 23:22:48 +0900

    .

--- app/src/main/java/com/example/dive/presentation/DiveControlButtons.kt
+++ app/src/main/java/com/example/dive/presentation/DiveControlButtons.kt
@@ -1,9 +1,10 @@
 package com.example.dive.presentation
 
-import androidx.compose.material.Button
-import androidx.compose.material.Text
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.foundation.layout.*
+import androidx.compose.ui.Modifier
 
 @Composable
 fun DiveControlButtons(
